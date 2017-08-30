42713e8d0f238df3183d874415640c15792d355e
WenWenDMA <31291674+WenWenDMA@users.noreply.github.com>
2017-08-30 12:10:35 -0700

    Update and rename WenWen_Reading1 to WenWen_Reading1.d

---
+++ assignments/WenWen_Reading1.d
@@ -0,0 +1,26 @@
+
+
+
+
+	In the article, “Processing/Lampreys-Photo-Fiction as Digital Parasite Aesthetics,” author Yvette Granata compares the photo-fictional lamprey with photographic image of lamprey. Granata points out that the processing lamprey produces the real lamprey because it is an more objective way of showing what a lamprey is without external influence. When people search lamprey on the Internet, Google answers what a lamprey looks like directly by showing photographs, descriptions, and definitions. If people have never seen lamprey before, their impression on lamprey would be depended on the image provided by Google and photographers. Thus, Granata considers that the Google system and photographers are the main subject of the image even though they both pretend that they disappear in the image. She thinks that the image of lamprey is a critique of lamprey as a parasite based on who stand behind the image telling people what they want people to see and who look at the image deciding how they want to interpret from the image. 
+
+	However, according to François Laquelle, photo-fiction can reveal an “internal operation” similar to the lamprey and rendering the real lamprey without an “external realism.” Based on Laquelle’s philosophy and definition on photo-fiction, Granata introduces a philosophy of parasite aesthetics. According to Granata, using processing coding language, just like the way that lamprey lives, the image of lamprey becomes “conceptual-photographic act,” which generates by itself and “crafts a realism in its underdetermined world.” Comparing to photographic image which uses its prerogative to tell people the realness, Granata thinks that parasite aesthetics is transcendence. She suggests that parasite aesthetics shows the revelation of objects as it is doing what a parasite does. It is independent from the outside world and “parallel” to the world, at the same time, produce its own realism. 
+
+	If photo-fictional parasite is a different realism of photography, what is real? Before I read this article, I did not know lamprey and how it looks like. My first impression and understanding on lamprey is based on Granata’s animation still image and video. Then I compare the photographs on Google, I think my impressions on both sources are subjective. Meanwhile, both sources have their own ways to subject the image of lamprey. When I use Processing, I feel that I have control in many ways to produce what I want viewers to perceive. The philosophy of parasite aesthetics might not apply and work in any circumstances, however, it shows the infinite possibilities of processing and new media art. People need to question on what we are really looking at and what do we want to look at in a lamprey image. 
+
+Reference: 
+Granata, Yvette. "Processing/Lampreys." Ctrl-Z: New Media Philosophy. 2016. Accessed August 30, 2017. http://www.ctrl-z.net.au/articles/issue-6/granata-processing-lampreys/.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
